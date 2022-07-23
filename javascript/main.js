23f5887415005b6982aa4bf4b77c57d26ad099fb
Jonathan Gomez <jurielgomez_1993@hotmail.com>
2022-07-23 15:55:24 -0300

    agrego clases constructoras, los array de objetos y dinamizo los precios del switch

--- javascript/main.js
+++ javascript/main.js
@@ -1,39 +1,56 @@
 //Mi proyecto va a estar enfocado en darle funcionalidad a una pagina web de eloboost y merch de League Of Legends. Por lo tanto, usaré eso como "inspiración" para el código. En este caso, trabajo el proceso de compra de la parte de merch, declarando los tres productos que estarán disponibles.
 
-// do{
-//     producto = prompt("¿Qué producto desea comprar?" +"\nAlmohadones ($1000)\nRemeras ($2000)\nFiguras ($500)")
-//     cantidad = parseInt(prompt("¿Cuántas unidades desea?"))
+//AGREGADO DE DESAFIO COMPLEMENTARIO II:
+//- VOY A DECLARAR EL CONSTRUCTOR DE OBJETOS PARA LOS TRES TIPOS DE PRODUCTOS QUE SE OFRECEN (REMERAS-ALMOHADONES-FIGURAS)
+//- VOY A AGREGAR DICHOS OBJETOS CON "NEW", ALOJANDO CADA UNO EN SU ARRAY CORRESPONDIENTE
+//- Reemplazaré los precios por los dinámicos de los array en el switch.
 
-//     let precio;
+class Almohadon{
+    constructor (modelo, tamaño, precio, stock){
+    this.modelo = modelo;
+    this.tamaño = tamaño;
+    this.precio = precio;
+    this.stock = stock;
+}
+}
 
-//     switch (producto) {
-//     case "Remeras":
-//         precio = 2000;
-//         break;
-//     case "Almohadones":
-//         precio = 1000;
-//         break;
-//     case "Figuras":
-//         precio = 500;
-//         break;
-//     default:
-//         alert("Tiene que elegir uno de esos productos")
-//         precio= 0;
-//     break;
-//     }
+class Remera{
+    constructor (modelo, talle, precio, stock){
+    this.modelo = modelo;
+    this.talle = talle;
+    this.precio = precio;
+    this.stock = stock;
+}
+}
 
-//     total += precio * cantidad;
+class Figura{
+    constructor (modelo, precio, stock){
+    this.modelo = modelo;
+    this.precio = precio;
+    this.stock = stock;
+}
+}
 
-//     seguirComprando = confirm("¿Desea comprar otro producto?");
-// } while (seguirComprando)
+const almohadones = [
+    almohadonGenerico = new Almohadon ("generico", "medium", 1000, 50),
+    yuumiGunAlmohadon = new Almohadon ("YuumiGun", "small", 2500, 0),
+    yuumiLibroAlmohadon = new Almohadon ("YuumiLibro", "small", 2000, 150), 
+]
 
-// alert("El total de su compra es: " + total);
+yuumiGunAlmohadon.stock = 10; // Aumento a 10 el stock de uno de los almohadones
 
-// if(total > 5000){
-//     alert("Debido al monto de su compra, se le aplicó un 10% de descuento. Su nuevo total es: " + total * 0.9);
-// } else{
-//     alert("Muchas gracias por su compra! Procure pagar en efectivo (Si usted trabaja en la AFIP, esto es una broma) *guiño guiño*")
-// };
+const remeras = [
+    remeraGenerico = new Remera ("generico", "XL", 2000, 50),
+    yuumiGun = new Remera ("YuumiGun", "L", 2500, 30),
+    yuumiLibro = new Remera ("YuumiLibro", "S", 2000, 150),
+    yuumiChibi = new Remera ("YuumiChibi", "XL", 3000, 10),
+]
+
+const figuras = [
+    figuraGenerico = new Figura ("generico", 500, 50),
+    yuumiDirectora = new Figura ("YuumiDirectora", 5000, 5),
+    yuumiOlas = new Figura ("YuumiOlas", 800, 80),
+]
 
 let producto;
 let cantidad;
@@ -51,13 +68,13 @@ function procesoCompra (){
     
         switch (producto) {
         case "Remeras":
-            precio = 2000;
+            precio = remeraGenerico.precio;
             break;
         case "Almohadones":
-            precio = 1000;
+            precio = almohadonGenerico.precio;
             break;
         case "Figuras":
-            precio = 500;
+            precio = figuraGenerico.precio;
             break;
         default:
             alert("Tiene que elegir uno de esos productos")
